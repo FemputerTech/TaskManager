8ee09d734c043c4c1cdd4c4ea72a1f126f45f828
FemputerTech <mleicht92@gmail.com>
2024-09-05 10:47:59 -0700

    added helmet for the website title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import "./App.css";
+import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>To Do</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Sign Language AI App" />
+      </Helmet>
       <header>
         <Navbar />
       </header>
