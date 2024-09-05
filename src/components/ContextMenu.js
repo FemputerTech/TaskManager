f6673d76f9e0a94173571833de302dc7e0edcd04
FemputerTech <mleicht92@gmail.com>
2024-09-05 14:31:39 -0700

    added the menu options to the context menu

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "../styles/ContextMenu.css";
 
-const ContextMenu = ({ isToggled, contextMenuRef }) => {
+const ContextMenu = ({
+  contextMenuRef,
+  isToggled,
+  options,
+  positionX,
+  positionY,
+}) => {
   return (
     <menu
       ref={contextMenuRef}
       className={`context-menu ${isToggled ? "active" : ""}`}
-    ></menu>
+      style={{
+        top: positionY + 2 + "px",
+        left: positionX + 2 + "px",
+      }}
+    >
+      {options.map((option, index) => (
+        <button className="option" key={index}>
+          <span>{option.title}</span>
+          <box-icon name={option.icon} type="solid"></box-icon>
+        </button>
+      ))}
+    </menu>
   );
 };
 
