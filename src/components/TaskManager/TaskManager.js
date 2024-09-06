6389f8616be15c99ed656d500ec08d87ce6da622
FemputerTech <mleicht92@gmail.com>
2024-09-06 16:39:39 -0700

    setup task and adding tasks to task manager

--- src/components/TaskManager/TaskManager.js
+++ src/components/TaskManager/TaskManager.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 import "../../styles/TaskManager/TaskManager.css";
 
 function TaskManager({ workspace, updateWorkspaceTitle }) {
+  const [tasks, setTasks] = useState([]);
   const handleOnChange = (event) => {
     updateWorkspaceTitle(event.target.value);
+  };
+  const addTask = () => {
+    const newTask = { title: "Untitled" };
+    setTasks([...tasks, newTask]);
   };
   return (
     <div className="task-manager">
       {workspace ? (
-        <input
-          className="task-manager-title"
-          type="text"
-          value={workspace.title}
-          onChange={handleOnChange}
-          placeholder="Untitled"
-        />
+        <>
+          <input
+            className="task-manager-title"
+            type="text"
+            value={workspace.title}
+            onChange={handleOnChange}
+            placeholder="Untitled"
+          />
+          <button className="new-task-button" onClick={addTask}>
+            <span className="button-text">Add new task</span>
+          </button>
+          <ul className="tasks">
+            {tasks.map((task, index) => (
+              <Task />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>Select a workspace from the sidebar or create a new one</p>
       )}
