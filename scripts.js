c2ab4973715cbe695c8ab3a8c61c1e5632e404bd
FemputerTech <mleicht92@gmail.com>
2024-09-30 15:19:33 -0700

    add a project id and implemented the add method

--- scripts.js
+++ scripts.js
@@ -9,13 +9,29 @@ class Task {
 }
 
 class Project {
-  constructor() {
+  constructor(id) {
+    this.id = id;
     this.title = "New Title";
     this.category = "private";
     this.tasks = [];
   }
+  add() {
+    const projectListDiv = document.querySelector(".project-list");
+    const projectDiv = document.createElement("div");
+    projectDiv.className = "project";
+    projectDiv.innerHTML = `
+        <p>${this.title}</p>
+    `;
+    projectListDiv.appendChild(projectDiv);
+    console.log(projectListDiv);
+  }
 }
 
-document
-  .getElementById("add-project")
-  .addEventListener("click", () => console.log("adding a new project"));
+const projects = [];
+
+document.getElementById("add-project").addEventListener("click", () => {
+  const projectId = projects.length + 1;
+  const newProject = new Project(projectId);
+  newProject.add();
+  projects.push(newProject);
+});
